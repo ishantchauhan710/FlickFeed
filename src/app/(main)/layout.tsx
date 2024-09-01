65bf1a4906b506b25105a27ca8f84356dcfdb3e2
ishantchauhan710 <ishantchauhan710@gmail.com>
2024-09-01 12:07:06 +0530

    Side bar added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 export default async function Layout({
   children,
@@ -18,7 +19,11 @@ export default async function Layout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
