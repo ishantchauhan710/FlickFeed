dea4ec499c190eb53ba07167b9ded025bbd847e8
ishantchauhan710 <ishantchauhan710@gmail.com>
2024-09-02 16:58:18 +0530

    Update query cache on new post added feature implemented

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -8,9 +8,13 @@ import UserAvatar from "@/components/UserAvatar";
 import { useSession } from "@/app/(main)/SessionProvider";
 import { Button } from "@/components/ui/button";
 import "./styles.css";
+import { useSubmitPostMutation } from "./mutations";
+import LoadingButton from "@/components/LoadingButton";
 
 export default function PostEditor() {
   const { user } = useSession();
+
+  const mutation = useSubmitPostMutation();
 
   const editor = useEditor({
     extensions: [
@@ -29,28 +33,36 @@ export default function PostEditor() {
       blockSeparator: "\n",
     }) || "";
 
-  async function onSubmit() {
-    await submitPost(input);
-    editor?.commands.clearContent();
+  function onSubmit() {
+    mutation.mutate(input, {
+      onSuccess: () => {
+        editor?.commands?.clearContent();
+      },
+    });
   }
 
   return (
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
-        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" size={36} />
+        <UserAvatar
+          avatarUrl={user.avatarUrl}
+          className="hidden sm:inline"
+          size={36}
+        />
         <EditorContent
           editor={editor}
           className="max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3"
         />
       </div>
       <div className="flex justify-end">
-        <Button
+        <LoadingButton
           onClick={onSubmit}
           disabled={!input.trim()}
           className="min-w-20"
+          loading={mutation.isPending}
         >
           Post
-        </Button>
+        </LoadingButton>
       </div>
     </div>
   );
---
+++ src/components/posts/editor/mutations.ts
@@ -0,0 +1,57 @@
+import { useToast } from "@/components/ui/use-toast";
+import {
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+  InfiniteData,
+} from "@tanstack/react-query";
+import { submitPost } from "./actions";
+import { PostsPage } from "@/lib/types";
+
+export function useSubmitPostMutation() {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: submitPost,
+    onSuccess: async (newPost) => {
+      const queryFilter: QueryFilters = {
+        queryKey: ["post-feed", "for-you"],
+      };
+      await queryClient.cancelQueries(queryFilter); // Cancel current running query
+
+      // Update posts cache with our new post
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          const firstPage = oldData?.pages[0];
+          if (firstPage) {
+            return {
+              pageParams: oldData.pageParams,
+              pages: [
+                {
+                  posts: [newPost, ...firstPage.posts],
+                  nextCursor: firstPage.nextCursor,
+                },
+                ...oldData.pages.slice(1),
+              ],
+            };
+          }
+        },
+      );
+
+      toast({
+        description: "Post created",
+      });
+    },
+    onError(error) {
+      console.log(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to post. Please try again",
+      });
+    },
+  });
+  return mutation;
+}
