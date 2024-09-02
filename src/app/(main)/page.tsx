3630bc4fad3c262916b3e3246bb3b5efedfced80
ishantchauhan710 <ishantchauhan710@gmail.com>
2024-09-02 22:35:27 +0530

    Get feeds of followings feature added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,16 +2,28 @@ import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 import Image from "next/image";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
   return (
     <main className="m-2 flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
---
+++ src/app/api/posts/following/route.ts
@@ -0,0 +1,57 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude } from "@/lib/types";
+import { NextRequest } from "next/server";
+import { PostsPage } from "@/lib/types";
+
+export async function GET(req: NextRequest) {
+  try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const pageSize = 10;
+
+    const { user } = await validateRequest();
+    if (!user) {
+      return Response.json(
+        { error: "Unauthorized" },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    const posts = await prisma.post.findMany({
+      where: {
+        user: {
+          followers: {
+            some: {
+              followerId: user.id, // Find posts of users where user.followers contain loggedIn user
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined, // cursor is id of the next post after results, eg 10th post, cursor = 11th post.id
+      include: getPostDataInclude(user.id),
+    });
+
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: "Internal server error" },
+      {
+        status: 500,
+      },
+    );
+  }
+}
